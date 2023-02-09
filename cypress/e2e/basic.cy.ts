b727dbb24de4ae7e21b9960bd807dddc2d84f681
Jesper Eriksson <84853548+jeppeerixon@users.noreply.github.com>
2023-02-09 13:39:58 +0100

    :tada: added a few test and prep mock data

--- cypress/e2e/basic.cy.ts
+++ cypress/e2e/basic.cy.ts
@@ -1,9 +1,60 @@
 
-it("should find search input", () => {
-    cy.visit("http://localhost:1234");
+beforeEach("load local host", () => {
+    cy.visit("/");
+});
 
-    cy.get("input").type("joker").should("have.value", "joker");
+describe("Tests for our happy flow", () => {
 
-    cy.get("button:first").click();
+    it("should find search input, type and get and results", () => {
+        cy.get("input").type("joker").should("have.value", "joker");
+    
+        cy.get("button:first").click();
 
-})
+        cy.get(".movie").should("have.length", 10);
+
+        cy.get(".movie:first > h3").should("contain.text", "Joker");
+    
+    });
+
+    it("should find search input, type and get results using keyboard", () => {
+        cy.get("input").type("batman").should("have.value", "batman").type('{enter}');
+
+        cy.get(".movie").should("have.length.at.least", 9);
+
+        cy.get(".movie:first-child > h3").should("contain.text", "Batman Begins");
+    
+    });
+
+});
+
+describe("Tests for our not so happy flow", () => {
+    it("should not find any search results", () => {
+
+        cy.get("#searchText").type("borde inte hitta någon film").should("have.value", "borde inte hitta någon film");
+    
+        cy.get("#search").click();
+
+        cy.get("p").should("contain.text", "Inga sökresultat att visa");
+
+    });
+
+});
+
+describe("Tests using mock data", () => {
+    it("should not find any search results", () => {
+
+        cy.get("#searchText").type("borde inte hitta någon film").should("have.value", "borde inte hitta någon film");
+    
+        cy.get("#search").click();
+
+        cy.get("p").should("contain.text", "Inga sökresultat att visa");
+
+    });
+
+});
+
+
+
+// lägg till base url
+
+// lätt till before each till url:en
